9866cf7f21f3b74abf857bc2919f7c728eb08627
sslava123 <sslavinskiy@me.com>
2026-04-03 21:39:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-
+var CACHE='z7ii-scan-v1';
+var FILES=['./','./index.html'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
